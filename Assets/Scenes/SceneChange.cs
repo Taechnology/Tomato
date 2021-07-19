0123a02ad71e80c19b195140366f707487f47589
Gaeun Lee <student@Gaeun-Lees-MacBook.local>
2021-07-19 23:40:34 +0900

    Created panel, scene 1&2, and scene changing script

---
+++ Assets/Scenes/SceneChange.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void NextScene(string next_scene)
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+
+}
